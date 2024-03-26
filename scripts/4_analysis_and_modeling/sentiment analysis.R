89b2efa4d5ea7f2d63f9134973713b512ed5e5f8
ThijsVroegh <thijs.vroegh@outlook.com>
2024-03-26 22:40:52 +0100

    added saving of sentiment analyses figures in tiff

--- scripts/4_analysis_and_modeling/sentiment analysis.R
+++ scripts/4_analysis_and_modeling/sentiment analysis.R
@@ -16,10 +16,12 @@ library(lubridate)
 events <- read.csv("data/external/historical events.csv",sep = ";") %>%
     mutate(Start.Date = mdy(Start.Date),
            End.Date = mdy(End.Date))
+
            #End.Date = ifelse(is.na(End.Date),Start.Date,End.Date))
 
-# data with topic analytical results
-texts <- readRDS("data/processed/founders/texts.rds") %>%
+# read in data with topic-analytical results
+#texts <- readRDS("data/processed/founders/texts.rds") %>%
+texts <- read.csv("data/processed/founders/letters_with_topic_info.csv") %>%
     mutate(sending_date = as_date(sending_date)) %>%
     mutate(year_month = floor_date(sending_date, unit = "month"))
 
@@ -31,18 +33,15 @@ rects <- data.frame(xstart = c(as.Date('1725-01-01'),as.Date('1775-04-19'),as.Da
 
 # ensure a dataframe with ids and each word is in a separate row
 # use year_month instead of Year when wanting to plot per month
-df_texts <- texts %>% select(id,Year,topic,text_cleaned) %>%
+df_texts <- texts %>% select(id,year,topic,text_cleaned) %>%
     separate_rows(text_cleaned) %>%
     rename(word = text_cleaned)
-
-# check
-#https://programminghistorian.org/en/lessons/sentiment-analysis-syuzhet
 
 # calculate sentiment per topic per year
 # AFINN ----
 sentiment_per_year <- df_texts %>%
     inner_join(get_sentiments("afinn"), by = "word") %>%
-    group_by(topic, Year) %>%
+    group_by(topic, year) %>%
         summarize(sentiment = mean(value),
                   words = n()) %>%
     ungroup()
@@ -51,26 +50,32 @@ sentiment_per_year <- df_texts %>%
     #    mutate(mean_value = mean(sentiment)) %>%
     #ungroup()
 
-# Sentiment development for individual topic ----
+# Sentiment development for individual topic (e.g., 01_Liberal politics or
+# "02_Republican politics") ----
+tiff(filename = "output/figures/Sentiment Republican politics.tiff", width = 8000, height = 4000, res = 450)
+
 ggplot(sentiment_per_year %>%
-    filter(topic =="revolutionary politics"), aes(Year, sentiment, fill = as.factor(topic))) +
+    filter(topic == "02_Republican politics"), aes(year, sentiment, fill = as.factor(topic))) +
     geom_col(show.legend = FALSE) +
     facet_wrap(~as.factor(topic), ncol = 2, scales = "free_x")
+dev.off()
 
 # We can see in the Figure how the plot changes toward more positive or negative
 # sentiment over the trajectory of the eighteenth century.
 
+tiff(filename = "output/figures/Trendline sentiment Liberal politics.tiff", width = 8000, height = 4000, res = 450)
 sentiment_per_year %>%
-    filter(topic == "revolutionary politics") %>%
-    ggplot(aes(x = make_date(Year, 1, 1), y = sentiment)) +
+    filter(topic == "01_Liberal politics") %>%
+    ggplot(aes(x = make_date(year, 1, 1), y = sentiment)) +
     geom_line() +
     geom_smooth(method = "loess", se = F, color = "red", span = 0.5) +
-    geom_line(aes(y = zoo::rollmean(sentiment, 5, fill = NA)), color = "blue", linetype = "solid") +
+    #geom_line(aes(y = zoo::rollmean(sentiment, 5, fill = NA)), color = "blue", linetype = "solid") +
     theme_ridges() +
     theme_minimal() +
-    geom_vline(data = events, aes(xintercept = Start.Date),
-               linetype = "solid", color = "light gray") +
+    #geom_vline(data = events, aes(xintercept = Start.Date),
+    #           linetype = "solid", color = "light gray") +
     guides(color = guide_legend(title = "Legend Title"))
+dev.off()
 
 # Sentiment development for all topics ----
 
@@ -81,12 +86,15 @@ color_palette <- viridis::viridis_pal()(num_topics)
 # Plot ridge graph with sentiment development for all topics per year
 # We can see in the Figure how the plot changes toward more positive or negative
 # sentiment over the trajectory of the eighteenth century.
-ggplot(sentiment_per_year, aes(x = Year, height = sentiment, y = as.factor(topic),fill = as.factor(topic))) +
+
+tiff(filename = "output/figures/sentiment all topics.tiff", width = 8000, height = 4000, res = 450)
+ggplot(sentiment_per_year, aes(x = year, height = sentiment, y = as.factor(topic),fill = as.factor(topic))) +
     geom_ridgeline(min_height = -100, scale = 1/2,color = "light gray") +
     scale_fill_manual(values = color_palette) +
     guides(fill = "none") +
     ylab("Topical sentiment") +
     xlab("Date")
+dev.off()
 
 # Wordcloud plots positive versus negative ----
 df_texts %>%
